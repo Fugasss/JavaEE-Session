8f7b6c0267310c167b98a2e9d3204b02187356f4
Fugasss <krip.1.101@gmail.com>
2024-10-31 18:30:37 +0500

    Properties configuration

--- backend/src/main/java/com/plunker/backend/BackendApplication.java
+++ backend/src/main/java/com/plunker/backend/BackendApplication.java
@@ -2,12 +2,14 @@ package com.plunker.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // excluding should be deleted after postgresql configuring
+@EnableMongoRepositories
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
