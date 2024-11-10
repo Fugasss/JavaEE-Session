e0b0db53b737cc8a98cbbe2a8d22f9a2b33c7278
Joopenzie <unitymaster60@gmail.com>
2024-11-10 21:27:32 +0500

    Контроллер! С фильтрами!

--- backend/src/main/java/com/plunker/backend/basket/controllers/ProductController.java
+++ backend/src/main/java/com/plunker/backend/basket/controllers/ProductController.java
@@ -2,6 +2,8 @@ package com.plunker.backend.basket.controllers;
 
 import com.plunker.backend.basket.models.Product;
 import com.plunker.backend.basket.services.ProductService;
+import com.plunker.backend.basket.models.Component;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -19,8 +21,42 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping("")
+    @GetMapping("/all")
     public Page<Product> GetAllProducts(@RequestParam int page, @RequestParam int size) {
         return productService.getAllProducts(page, size);
     }
+
+    @GetMapping("")
+    public Page<Product> GetProducts(
+        @RequestParam(defaultValue = "1") int page, 
+        @RequestParam(defaultValue = "1") int size,
+        @RequestParam(defaultValue = "0") int min,
+        @RequestParam(defaultValue = "-1") int max,
+        @RequestParam(defaultValue = "default") String type) {
+        
+        Component component = null;
+        boolean defaultType = true;
+
+        try {
+            component = Component.valueOf(type);
+        } catch (IllegalArgumentException e) {
+            defaultType = false;
+        }
+
+        if (type.equals("deault") || defaultType == false){
+            if (max == -1) {
+                return productService.getAllProducts(page, size);
+            } else {
+                return productService.getProductsByPriceRangeAsc(min, max, page, size);
+            }
+        } else {
+
+            if (max == -1) {
+                return productService.getProductsByPriceAsc(page, size, component);
+            } else {
+                return productService.getProductsByPriceAndTypeAsc(min, max, component, page, size);
+            }
+        }
+    }
+    
 }
