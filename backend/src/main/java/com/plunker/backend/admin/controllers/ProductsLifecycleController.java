fb54e97a25072c87e47164d4e8149dd8a08df1ba
Fugasss <krip.1.101@gmail.com>
2024-11-10 19:35:56 +0500

    Intermediate changes

---
+++ backend/src/main/java/com/plunker/backend/admin/controllers/ProductsLifecycleController.java
@@ -0,0 +1,14 @@
+package com.plunker.backend.admin.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("admin/product/")
+@RequiredArgsConstructor
+public class ProductsLifecycleController {
+
+
+}
