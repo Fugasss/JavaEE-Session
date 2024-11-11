15026a04e38acf36da98ac3f7a25bc93ea5ca85e
Joopenzie <unitymaster60@gmail.com>
2024-11-11 12:55:14 +0500

    Shet

--- backend/src/main/java/com/plunker/backend/basket/controllers/OrderController.java
+++ backend/src/main/java/com/plunker/backend/basket/controllers/OrderController.java
@@ -4,12 +4,15 @@ import com.plunker.backend.auth.services.UserService;
 import com.plunker.backend.basket.models.Order;
 import com.plunker.backend.basket.models.Product;
 import com.plunker.backend.basket.services.OrderService;
+import com.plunker.backend.basket.services.ProductService;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,11 +26,24 @@ public class OrderController {
 
     private final OrderService orderService;
     private final UserService userService;
+    private final ProductService productService;
 
     @GetMapping("/all")
     public Page<Order> GetAllProducts(
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "1") int size) {
         return orderService.getAllOrdersByUserID(userService.getCurrentUser().getId(), page, size);
+    }
+
+    @PostMapping("/new")
+    public void AddOrder(@RequestParam String productID) {
+        var order = Order.builder()
+        .sold(false)
+        .userId(userService
+        .getCurrentUser()
+        .getId())
+        .product(productService.getProductById(productID))
+        .build();
+        orderService.createOrder(order);
     }
 }
--- backend/src/main/java/com/plunker/backend/basket/repositories/ProductRepository.java
+++ backend/src/main/java/com/plunker/backend/basket/repositories/ProductRepository.java
@@ -4,7 +4,11 @@ import com.plunker.backend.basket.models.Product;
 import com.plunker.backend.basket.models.Component;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+<<<<<<< Updated upstream
 import org.springframework.data.jpa.repository.JpaRepository;
+=======
+import org.springframework.data.repository.PagingAndSortingRepository;
+>>>>>>> Stashed changes
 import org.springframework.stereotype.Repository;
 
 
