9249332d348c5336006ccbd72c4cf358ee6745b6
Fugasss <krip.1.101@gmail.com>
2024-11-17 13:01:08 +0500

    YESS, YESS

--- backend/src/main/java/com/plunker/backend/basket/dto/OrderRequest.java
+++ backend/src/main/java/com/plunker/backend/basket/dto/OrderRequest.java
@@ -1,9 +1,13 @@
 package com.plunker.backend.basket.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.UUID;
 
 @Data
 public class OrderRequest {
-    public String productId;
 
+    @NotBlank
+    @UUID
+    private String productId;
 }
