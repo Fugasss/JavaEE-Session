4015b43e7b7c78de291e1ff4e0edd902afece916
Joopenzie <unitymaster60@gmail.com>
2024-11-17 13:44:56 +0500

    Update ProductController.java

--- backend/src/main/java/com/plunker/backend/basket/controllers/ProductController.java
+++ backend/src/main/java/com/plunker/backend/basket/controllers/ProductController.java
@@ -1,5 +1,8 @@
 package com.plunker.backend.basket.controllers;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.plunker.backend.basket.models.Product;
 import com.plunker.backend.basket.services.ProductService;
 import com.plunker.backend.basket.models.Component;
@@ -7,6 +10,7 @@ import com.plunker.backend.basket.models.Component;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,10 +36,12 @@ public class ProductController {
         @RequestParam(defaultValue = "1") int size,
         @RequestParam(defaultValue = "0") int min,
         @RequestParam(defaultValue = "-1") int max,
-        @RequestParam(defaultValue = "default") String type) {
+        @RequestParam(defaultValue = "default") String type,
+        @RequestParam(defaultValue = "default") String tag) {
         
         Component component = null;
         boolean defaultType = true;
+        Page<Product> result;
 
         try {
             component = Component.valueOf(type);
@@ -45,18 +51,35 @@ public class ProductController {
 
         if (type.equals("default") || defaultType == false){
             if (max == -1) {
-                return productService.getAllProducts(page, size);
+                result = productService.getAllProducts(page, size);
             } else {
-                return productService.getProductsByPriceRangeAsc(min, max, page, size);
+                result = productService.getProductsByPriceRangeAsc(min, max, page, size);
             }
         } else {
 
             if (max == -1) {
-                return productService.getProductsByPriceAsc(page, size, component);
+                result = productService.getProductsByPriceAsc(page, size, component);
             } else {
-                return productService.getProductsByPriceAndTypeAsc(min, max, component, page, size);
+                result = productService.getProductsByPriceAndTypeAsc(min, max, component, page, size);
             }
         }
+
+        if(!tag.equals("default")) {
+            List<Product> container = result.getContent();
+
+            for(int i = container.size() - 1; i >= 0; i--) {
+                Pattern pattern = Pattern.compile(".+" + tag + ".+", Pattern.CASE_INSENSITIVE);
+                Matcher matcher = pattern.matcher(container.get(i).getProductInfo());
+                
+                if(!matcher.find()) {
+                    container.remove(i);
+                }
+            }
+            
+            result = new PageImpl<Product>(container);
+        }
+
+        return result;
     }
     
 }
