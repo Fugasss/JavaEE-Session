2220d5532b068720e36c4726cf6ba86d846056a1
Fugasss <krip.1.101@gmail.com>
2024-11-28 16:41:43 +0500

    Added uploading and downloading users' profile images

--- backend/src/main/java/com/plunker/backend/BackendApplication.java
+++ backend/src/main/java/com/plunker/backend/BackendApplication.java
@@ -1,7 +1,9 @@
 package com.plunker.backend;
 
+import com.plunker.backend.userprofile.services.storages.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -9,6 +11,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @EnableMongoRepositories
 @EnableJpaRepositories
+@EnableConfigurationProperties({StorageProperties.class})
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
